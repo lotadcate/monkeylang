bbb969d75eee9dfa94515196ebbf2e8935d6d4da
beetroot <btrt@dev.lan>
2024-07-27 06:16:10 +0900

    トークンタイプの定義

--- token/token.go
+++ token/token.go
@@ -1,7 +1,45 @@
 package token
 
+/*
+トークンには種類がある（タイプ）
+トークンはタイプとリテラル（トークンの個別の情報）が必要
+
+let five = 5;
+let ten = 10;
+let add = fn(x,y) {
+  x + y;
+}
+let result = add(five, ten);
+*/
 type TokenType string
 type Token struct {
 	Type TokenType
 	Literal string
 }
+
+const (
+	// 識別子
+	IDENT = "IDENT" // five, ten, add, x, y
+
+	// リテラル
+	INT = "INT" // 5, 10
+
+	// キーワード
+	FUNCTION = "FUNCTION" // fn
+	LET = "LET" // let
+
+	// 演算子
+	ASSIGN = "="
+	PLUS = "+"
+
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+)
